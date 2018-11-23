92bc12d87745af89dcb24d3ab4040d8e26ffe018
Antonio DiNostri <semiviral@gmail.com>
2018-11-23 01:32:56 -0600

    initial commit

--- PingAggregator/Program.cs
+++ PingAggregator/Program.cs
@@ -1,9 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
 
 namespace PingAggregator {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+    public static class Program {
+        private static List<IPAddress> Addresses { get; set; }
+
+        private static void Main(string[] args) {
+            Addresses = new List<IPAddress>();
+            Ping ping = new Ping();
+
+            while (true) {
+                PingReply pingReply;
+
+                do {
+                    pingReply = ping.Send("nl.torguardvpnaccess.com");
+                } while (!pingReply.Status.Equals(IPStatus.Success));
+
+                if (Addresses.Contains(pingReply.Address)) {
+                    continue;
+                }
+
+                Addresses.Add(pingReply.Address);
+                WriteAddresses();
+
+                Thread.Sleep(30000);
+            }
+        }
+
+        private static void WriteAddresses() {
+            Console.Clear();
+
+            foreach (string address in Addresses.Select(arg => Version.Parse(arg.ToString())).OrderBy(arg => arg).Select(arg => arg.ToString())) {
+                Console.WriteLine(address);
+            }
         }
     }
 }
